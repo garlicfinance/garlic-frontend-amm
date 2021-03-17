7fb8952b5633cfddf11ba65375ffa2e2471ef727
garlicfinance <garlicfinance@gmail.com>
2021-03-17 23:39:23 +0300

    fixed infinity

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x09eb8438451c1C617b6051EED015db988af6Df2A',
+  cakeAddress: '0x0d8ec18c2339088426d213CD72ADeF9c637a394E',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0xf1aD888BeD4FE7bA3c7dBF9Bf2F15D01aAd7975C'
 }
